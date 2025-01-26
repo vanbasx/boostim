2fc9785702e34d31427c1b8249a6bd39670db856
vanbasx <ivan.bas.vb@gmail.com>
2025-01-26 21:03:22 +0100

    up

---
+++ theme/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+// Styles and scripts import
+function boostim_enqueue_scripts() {
+
+  wp_enqueue_style(
+    'boostim-style',
+    get_parent_theme_file_uri('assets/css/style.css'),
+    array(),
+    wp_get_theme()->get( 'Version' ), 'all'
+  );
+
+  wp_enqueue_script(
+    'boostim-script',
+    get_parent_theme_file_uri( 'assets/js/index.js' ),
+    array(),
+    wp_get_theme()->get( 'Version' ), 
+    true
+  );
+
+  if (!is_user_logged_in()) {
+    wp_dequeue_style('wp-block-library');
+  }
+
+}
+add_action('wp_enqueue_scripts', 'boostim_enqueue_scripts');
---
+++ theme/index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="bg-red-100">
+  index-page
+</div>
+
+<?php get_footer(); ?>
