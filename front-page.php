ebbe2d6d7ed7b94cf285d5e9010a6dc9e6916841
vanbasx <ivan.bas.vb@gmail.com>
2025-02-04 20:16:43 +0100

    up

--- front-page.php
+++ front-page.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 
 <section class="mt-16 max-[1024px]:mt-5 mb-36 max-[1024px]:mb-28 max-[570px]:mb-15">
-  <div class="flex max-[768px]:flex-col max-[1024px]:items-center justify-between gap-15 max-[1024px]:gap-6 max-[1240px]:gap-8 container">
+  <div class="flex max-[768px]:flex-col justify-between max-[1024px]:items-center gap-15 max-[1024px]:gap-6 max-[1240px]:gap-8 container">
 
     <div class="w-full hero">
-      <h1 class="text-7xl text-balance text-primary max-[430px]:text-4xl max-[570px]:text-5xl max-[1240px]:text-6xl max-[1024px]:text-5xl max-[768px]:text-6xl font-bold leading-[1.1] tracking-tight max-[1240px]:tracking-tighter mb-10 max-[1240px]:mb-7 max-[430px]:mb-5">
+      <h1 class="mb-10 max-[1240px]:mb-7 max-[430px]:mb-5 font-bold text-7xl text-balance text-primary max-[430px]:text-4xl max-[570px]:text-5xl max-[1240px]:text-6xl max-[1024px]:text-5xl max-[768px]:text-6xl leading-[1.1] tracking-tight max-[1240px]:tracking-tighter">
         Stay ahead of the curve with our forward-thinking
       </h1>
-      <p class="text-balance max-[430px]:text-pretty mb-10 max-[430px]:mb-7 subtitle">
+      <p class="mb-10 max-[430px]:mb-7 text-balance max-[430px]:text-pretty subtitle">
         An award-winning SEO agency with disciplines in digital marketing, design, and website development. Focused on understanding you.
       </p>
       <div class="flex items-center gap-12 max-[1024px]:gap-8 mb-22 max-[1240px]:mb-12 max-[768px]:mb-10">
-        <a href="" class="flex items-center gap-10 max-[1024px]:gap-2 max-[1024px]:text-sm bg-primary px-8 max-[1024px]:px-5 py-4 max-[1024px]:py-3 rounded-full">
-          <span class="text-white font-bold leading-[1.4]">Schedule Call</span>
+        <a href="" class="flex items-center gap-10 max-[1024px]:gap-2 bg-primary px-8 max-[1024px]:px-5 py-4 max-[1024px]:py-3 rounded-full max-[1024px]:text-sm">
+          <span class="font-bold text-white leading-[1.4]">Schedule Call</span>
           <svg class="max-[1024px]:size-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 5L19 12L12 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
         </a>
-        <a href="" class="text-primary max-[1024px]:text-sm font-bold underline">
+        <a href="" class="font-bold text-primary max-[1024px]:text-sm underline">
           View Case Study
         </a>
       </div>
       <div class="flex max-[1240px]:flex-col items-center max-[1240px]:items-start max-[768px]:items-center gap-12 max-[1240px]:gap-3">
-        <span class="text-primary text-sm max-[768px]:text-center font-bold w-[150px] max-[1240px]:w-full">
+        <span class="w-[150px] max-[1240px]:w-full font-bold text-primary text-sm max-[768px]:text-center">
           Trusted by the world's biggest brands
         </span>
         <div class="flex items-center gap-8 max-[1024px]:*:h-6">
@@ -40,22 +40,22 @@
           </div>
         </div>
         <div class="bg-gray p-6 max-[1024px]:p-3 max-[430px]:p-3 max-[768px]:p-6 pb-8 max-[1024px]:pb-5 max-[430px]:pb-5 max-[768px]:pb-8 rounded-[20px] w-full">
-          <p class="text-[84px] text-primary max-[570px]:text-5xl max-[1024px]:text-5xl max-[1240px]:text-6xl max-[768px]:text-[84px] font-bold leading-none mb-5 max-[1240px]:mb-3">
+          <p class="mb-5 max-[1240px]:mb-3 font-bold text-[84px] text-primary max-[570px]:text-5xl max-[1024px]:text-5xl max-[1240px]:text-6xl max-[768px]:text-[84px] leading-none">
             215+
           </p>
-          <p class="text-pretty max-[570px]:text-sm max-[1024px]:text-sm max-[768px]:text-base font-medium leading-[1.5] max-[1024px]:tracking-tight max-[768px]:tracking-normal mb-16 max-[1024px]:mb-7 max-[1240px]:mb-10 subtitle">
+          <p class="mb-16 max-[1024px]:mb-7 max-[1240px]:mb-10 font-medium text-pretty max-[570px]:text-sm max-[1024px]:text-sm max-[768px]:text-base leading-[1.5] max-[1024px]:tracking-tight max-[768px]:tracking-normal subtitle">
             some big companies that we work with, and trust us very much
           </p>
           <div class="relative before:top-0 before:left-0 before:absolute bg-[#D9D9D9] before:bg-primary w-full before:w-2/3 h-1.5 before:h-full"></div>
         </div>
       </div>
 
-      <div class="relative text-white bg-primary pt-12 max-[1024px]:pt-6 max-[430px]:pt-6 max-[768px]:pt-12 pr-68 max-[1024px]:pr-22 max-[1240px]:pr-32 max-[430px]:pr-20 max-[570px]:pr-32 max-[768px]:pr-57 pb-15 max-[1240px]:pb-20 max-[768px]:pb-15 pl-8 max-[1024px]:pl-5 max-[430px]:pl-5 max-[768px]:pl-8 rounded-[20px]">
+      <div class="relative bg-primary pt-12 max-[1024px]:pt-6 max-[430px]:pt-6 max-[768px]:pt-12 pr-68 max-[1024px]:pr-22 max-[1240px]:pr-32 max-[430px]:pr-20 max-[570px]:pr-32 max-[768px]:pr-57 pb-15 max-[1240px]:pb-20 max-[768px]:pb-15 pl-8 max-[1024px]:pl-5 max-[430px]:pl-5 max-[768px]:pl-8 rounded-[20px] text-white">
         <div class="flex items-center gap-3 mb-8 max-[1240px]:mb-2">
           <div class="bg-white w-13 max-[1024px]:w-5 max-[570px]:w-5 max-[768px]:w-13 h-px"></div>
           <p class="text-sm max-[570px]:text-sm max-[768px]:text-base">Drive More Traffic and Sales</p>
         </div>
-        <p class="text-3xl max-[768px]:text-3xl max-[1024px]:text-2xl max-[570px]:text-2xl max-[570px]:text-balance font-bold leading-[1.3] max-[430px]:tracking-tight w-2xs max-[1240px]:w-full">
+        <p class="w-2xs max-[1240px]:w-full font-bold text-3xl max-[768px]:text-3xl max-[1024px]:text-2xl max-[570px]:text-2xl max-[570px]:text-balance leading-[1.3] max-[430px]:tracking-tight">
           Drive more traffic and product sales 
         </p>
         <img class="right-5 bottom-0 absolute w-[227px] max-[1024px]:w-[130px] max-[1240px]:w-[180px] max-[570px]:w-[130px] max-[768px]:w-[200px]" src="<?php echo get_parent_theme_file_uri( 'assets/img/chart.png' ) ?>" alt="">
@@ -68,7 +68,7 @@
 
 <section class="!mb-36">
   <div class="container">
-    <div class="flex max-[1024px]:flex-col items-center max-[1024px]:items-start justify-between gap-10 max-[1024px]:gap-4 max-[570px]:gap-6 max-[768px]:gap-7 mb-18 max-[1024px]:mb-12">
+    <div class="flex max-[1024px]:flex-col justify-between items-center max-[1024px]:items-start gap-10 max-[1024px]:gap-4 max-[570px]:gap-6 max-[768px]:gap-7 mb-18 max-[1024px]:mb-12">
       <h2 class="max-[1024px]:text-pretty max-[1240px]:text-4xl max-[1240px]:text-balance max-[768px]:text-5xl max-[570px]:text-4xl max-[570px]:leading-[1.1] max-[570px]:tracking-tighter max-[1240px]:basis-1/2 max-w-2xl max-[1024px]:max-w-full title">
         Provide the best service with out of the box ideas
       </h2>
@@ -79,13 +79,13 @@
 
     <div class="flex max-[768px]:flex-col items-stretch gap-6 max-[1024px]:gap-4">
       <div class="bg-primary p-10 rounded-[30px]">
-        <div class="text-[84px] text-white max-[1240px]:text-6xl max-[1024px]:text-5xl max-[570px]:text-6xl max-[768px]:text-[84px] font-bold leading-none mb-4 max-[570px]:mb-2">
+        <div class="mb-4 max-[570px]:mb-2 font-bold text-[84px] text-white max-[1240px]:text-6xl max-[1024px]:text-5xl max-[570px]:text-6xl max-[768px]:text-[84px] leading-none">
           720<span class="text-accent">+</span>
         </div>
-        <p class="text-secondary text-xl max-[1024px]:text-base max-[1240px]:text-lg max-[570px]:text-base max-[768px]:text-xl font-medium mb-32 max-[1024px]:mb-28 max-[570px]:mb-18">
+        <p class="mb-32 max-[1024px]:mb-28 max-[570px]:mb-18 font-medium text-secondary text-xl max-[1024px]:text-base max-[1240px]:text-lg max-[570px]:text-base max-[768px]:text-xl">
           Project finish with superbly
         </p>
-        <div class="flex items-center gap-1 text-[84px] text-white max-[1240px]:text-6xl max-[1024px]:text-5xl max-[570px]:text-6xl max-[768px]:text-[84px] font-bold leading-none">
+        <div class="flex items-center gap-1 font-bold text-[84px] text-white max-[1240px]:text-6xl max-[1024px]:text-5xl max-[570px]:text-6xl max-[768px]:text-[84px] leading-none">
           <div class="border-[#737373] border-2 bg-gray rounded-full overflow-hidden size-18 max-[1240px]:size-15 max-[1024px]:size-13 max-[768px]:size-18 max-[570px]:size-15 max-[570px]:z-1"></div>
           <div class="border-[#737373] border-2 bg-gray rounded-full overflow-hidden size-18 max-[1240px]:size-15 max-[1024px]:size-13 max-[768px]:size-18 max-[570px]:size-15 max-[570px]:z-2 max-[570px]:-ml-4"></div>
           <div class="border-[#737373] border-2 bg-gray rounded-full overflow-hidden size-18 max-[1240px]:size-15 max-[1024px]:size-13 max-[768px]:size-18 max-[570px]:size-15 max-[570px]:z-3 max-[570px]:-ml-4"></div>
@@ -93,8 +93,8 @@
           +
         </div>
       </div>
-      <div class="relative flex items-center justify-center bg-[#C7C7C7] mr-6 max-[1024px]:mr-0 rounded-[30px] max-[768px]:aspect-square w-full">
-        <span class="max-[1024px]:hidden text-5xl text-white font-bold uppercase tracking-wider px-10">
+      <div class="relative flex justify-center items-center bg-[#C7C7C7] mr-6 max-[1024px]:mr-0 rounded-[30px] max-[768px]:aspect-square w-full">
+        <span class="max-[1024px]:hidden px-10 font-bold text-5xl text-white uppercase tracking-wider">
           HOW WE WORK
         </span>
         <button ontouchstart class="-right-6 max-[1024px]:right-1/2 -bottom-6 max-[1024px]:bottom-1/2 absolute rounded-full transition-transform max-[1024px]:translate-x-1/2 max-[1024px]:translate-y-1/2 duration-200 ease-linear cursor-pointer overflow-hidden shrink-0 hover:scale-105 active:scale-105">
--- functions.php
+++ functions.php
@@ -5,11 +5,14 @@ function boostim_theme_support() {
 
   add_theme_support( 'title-tag' );
   add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'html5' );
 
 }
 add_action( 'after_setup_theme', 'boostim_theme_support' );
 
-// Styles and scripts import
+
+
+// STYLES AND SCRIPTS REGISTER
 function boostim_enqueue_scripts() {
 
   wp_enqueue_style(
@@ -35,7 +38,9 @@ function boostim_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'boostim_enqueue_scripts' );
 
-// Fonts import
+
+
+// FONTS REGISTER
 function boostim_enqueue_fonts() {
 
   echo '<link rel="preload" href="' . get_parent_theme_file_uri( 'assets/fonts/Manrope-Bold.woff2' ) . '" as="font" type="font/woff2" crossorigin>';
@@ -43,3 +48,32 @@ function boostim_enqueue_fonts() {
 
 }
 add_action( 'wp_head', 'boostim_enqueue_fonts', 5 );
+
+
+
+// CPT REGISTER
+function boostim_custom_post_type() {
+
+  register_post_type('boostim_case',
+		array(
+			'labels'      => array(
+				'name'            => __('Case study', 'boostim'),
+				'singular_name'   => __('Case', 'boostim'),
+			),
+				'public'          => true,
+				'has_archive'     => true,
+        'rewrite'         => array( 'slug' => 'case-study' ),
+        'menu_icon'       => 'dashicons-portfolio',
+        'menu_position'   => 5,
+        'supports'        => array(
+          'title',
+          'editor',
+          'thumbnail',
+          'excerpt'
+        ),
+        'show_in_rest'    => true
+		)
+	);
+
+}
+add_action('init', 'boostim_custom_post_type');
