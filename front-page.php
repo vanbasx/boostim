8a0faadf90b978f1cbacc39ded59f651997f0406
vanbasx <ivan.bas.vb@gmail.com>
2025-02-12 22:25:02 +0100

    add images, field alt tags, add background images

--- front-page.php
+++ front-page.php
@@ -34,7 +34,7 @@
     <div data-da=".hero, 767, 3" class="max-[768px]:mb-10 pt-8 w-full">
 
       <div class="flex items-stretch gap-6 max-[1024px]:gap-2 max-[1240px]:gap-4 max-[430px]:gap-2 max-[570px]:gap-4 max-[768px]:gap-6 mb-6 max-[1024px]:mb-2 max-[1240px]:mb-4 max-[430px]:mb-2 max-[570px]:mb-4 max-[768px]:mb-6">
-        <div class="relative bg-[#C7C7C7] rounded-[100%_20px_20px_20px] w-full">
+        <div class="relative bg-[#C7C7C7] !bg-cover !bg-center rounded-[100%_20px_20px_20px] w-full" style="background: url('<?php echo get_parent_theme_file_uri( 'assets/img/hero.webp' ) ?>');">
           <div class="-top-8 right-8 absolute bg-primary p-7 rounded-full">
             <svg class="max-[1024px]:size-5 max-[1240px]:size-7" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M46 12L27 31L17 21L2 36" stroke="#A8D67B" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M34 12H46V24" stroke="#A8D67B" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>
           </div>
@@ -50,7 +50,11 @@
         </div>
       </div>
 
-      <div class="relative bg-primary pt-12 max-[1024px]:pt-6 max-[430px]:pt-6 max-[768px]:pt-12 pr-68 max-[1024px]:pr-22 max-[1240px]:pr-32 max-[430px]:pr-20 max-[570px]:pr-32 max-[768px]:pr-57 pb-15 max-[1240px]:pb-20 max-[768px]:pb-15 pl-8 max-[1024px]:pl-5 max-[430px]:pl-5 max-[768px]:pl-8 rounded-[20px] text-white">
+      <div 
+        style="background: url('<?php echo get_parent_theme_file_uri( 'assets/img/dark-background.webp' ) ?>')"
+        class="relative !bg-primary !bg-contain !bg-no-repeat pt-12 max-[1024px]:pt-6 max-[430px]:pt-6 max-[768px]:pt-12 pr-68 max-[1024px]:pr-22 max-[1240px]:pr-32 max-[430px]:pr-20 max-[570px]:pr-32 max-[768px]:pr-57 pb-15 max-[1240px]:pb-20 max-[768px]:pb-15 pl-8 max-[1024px]:pl-5 max-[430px]:pl-5 max-[768px]:pl-8 rounded-[20px] text-white"
+      >
+        <!-- <img class="top-0 left-0 absolute w-full h-full object-contain" src="<?php echo get_parent_theme_file_uri( 'assets/img/dark-background.webp' ) ?>" alt="background image"> -->
         <div class="flex items-center gap-3 mb-8 max-[1240px]:mb-2">
           <div class="bg-white w-13 max-[1024px]:w-5 max-[570px]:w-5 max-[768px]:w-13 h-px"></div>
           <p class="text-sm max-[570px]:text-sm max-[768px]:text-base">Drive More Traffic and Sales</p>
@@ -58,7 +62,7 @@
         <p class="max-[1240px]:w-full w-2xs font-bold max-[1024px]:text-2xl max-[570px]:text-2xl text-3xl max-[768px]:text-3xl max-[570px]:text-balance leading-[1.3] max-[430px]:tracking-tight">
           Drive more traffic and product sales 
         </p>
-        <img class="right-5 bottom-0 absolute w-[227px] max-[1024px]:w-[130px] max-[1240px]:w-[180px] max-[570px]:w-[130px] max-[768px]:w-[200px]" src="<?php echo get_parent_theme_file_uri( 'assets/img/chart.png' ) ?>" alt="">
+        <img class="right-5 bottom-0 absolute w-[227px] max-[1024px]:w-[130px] max-[1240px]:w-[180px] max-[570px]:w-[130px] max-[768px]:w-[200px]" src="<?php echo get_parent_theme_file_uri( 'assets/img/chart.png' ) ?>" alt="chart">
       </div>
 
     </div>
@@ -78,7 +82,10 @@
     </div>
 
     <div class="flex max-[768px]:flex-col items-stretch gap-6 max-[1024px]:gap-4">
-      <div class="bg-primary p-10 rounded-[30px]">
+      <div 
+        style="background: url('<?php echo get_parent_theme_file_uri( 'assets/img/dark-background.webp' ) ?>')"
+        class="!bg-primary !bg-contain !bg-no-repeat !bg-right p-10 rounded-[30px]"
+      >
         <div class="mb-4 max-[570px]:mb-2 font-bold text-[84px] text-white max-[768px]:text-[84px] max-[1024px]:text-5xl max-[1240px]:text-6xl max-[570px]:text-6xl leading-none">
           720<span class="text-accent">+</span>
         </div>
@@ -86,17 +93,20 @@
           Project finish with superbly
         </p>
         <div class="flex items-center gap-1 font-bold text-[84px] text-white max-[768px]:text-[84px] max-[1024px]:text-5xl max-[1240px]:text-6xl max-[570px]:text-6xl leading-none">
-          <div class="max-[570px]:z-1 bg-gray border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"></div>
-          <div class="max-[570px]:z-2 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"></div>
-          <div class="max-[570px]:z-3 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"></div>
-          <div class="max-[570px]:z-4 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"></div>
+          <div class="max-[570px]:z-1 bg-gray border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"><img class="w-full h-full object-cover" src="<?php echo get_parent_theme_file_uri( 'assets/img/person-1.webp' ) ?>" alt="person"></div>
+          <div class="max-[570px]:z-2 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"><img class="w-full h-full object-cover" src="<?php echo get_parent_theme_file_uri( 'assets/img/person-2.webp' ) ?>" alt="person"></div>
+          <div class="max-[570px]:z-3 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"><img class="w-full h-full object-cover" src="<?php echo get_parent_theme_file_uri( 'assets/img/person-3.webp' ) ?>" alt="person"></div>
+          <div class="max-[570px]:z-4 bg-gray max-[570px]:-ml-4 border-[#737373] border-2 rounded-full size-18 max-[1024px]:size-13 max-[1240px]:size-15 max-[570px]:size-15 max-[768px]:size-18 overflow-hidden"><img class="w-full h-full object-cover" src="<?php echo get_parent_theme_file_uri( 'assets/img/person-4.webp' ) ?>" alt="person"></div>
           +
         </div>
       </div>
       <div class="relative flex justify-center items-center bg-[#C7C7C7] mr-6 max-[1024px]:mr-0 rounded-[30px] w-full max-[768px]:aspect-square">
-        <span class="max-[1024px]:hidden px-10 font-bold text-white text-5xl uppercase tracking-wider">
+        <span class="max-[1024px]:hidden z-10 relative px-10 font-bold text-white text-5xl uppercase tracking-wider">
           HOW WE WORK
         </span>
+        <div class="top-0 left-0 absolute rounded-[30px] w-full h-full overflow-hidden">
+          <img class="brightness-50 w-full h-full object-cover" src="<?php echo get_parent_theme_file_uri( 'assets/img/how-we-work.webp' ) ?>" alt="group of people">
+        </div>
         <button ontouchstart class="-right-6 max-[1024px]:right-1/2 -bottom-6 max-[1024px]:bottom-1/2 absolute rounded-full overflow-hidden hover:scale-105 active:scale-105 transition-transform max-[1024px]:translate-x-1/2 max-[1024px]:translate-y-1/2 duration-200 ease-linear cursor-pointer shrink-0">
           <svg class="max-[1024px]:size-32 max-[1240px]:size-36" width="163" height="163" viewBox="0 0 163 163" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="6" y="6" width="151" height="151" rx="75.5" fill="#99EA48" stroke="#FAFAFA" stroke-width="12"/><path d="M98.9165 77.4811C99.797 77.9493 100.534 78.6483 101.047 79.5032C101.561 80.3581 101.832 81.3366 101.832 82.3339C101.832 83.3312 101.561 84.3097 101.047 85.1646C100.534 86.0195 99.797 86.7185 98.9165 87.1867L75.4278 99.9596C71.6457 102.017 67 99.3399 67 95.1086V69.5611C67 65.3261 71.6457 62.6512 75.4278 64.7064L98.9165 77.4811Z" fill="black"/></svg>
         </button>
@@ -106,7 +116,10 @@
 </section>
 
 <section id="case-study" class="mx-auto mb-31 max-[570px]:mb-15 px-4 max-w-[1408px]">
-  <div class="bg-primary px-16 max-[1024px]:px-8 max-[1240px]:px-13 max-[430px]:px-4 max-[768px]:px-6 py-20 max-[1240px]:py-15 max-[570px]:py-10 rounded-[30px] overflow-hidden">
+  <div 
+    style="background: url('<?php echo get_parent_theme_file_uri( 'assets/img/dark-background.webp' ) ?>')"
+    class="!bg-primary !bg-cover !bg-no-repeat !bg-right px-16 max-[1024px]:px-8 max-[1240px]:px-13 max-[430px]:px-4 max-[768px]:px-6 py-20 max-[1240px]:py-15 max-[570px]:py-10 rounded-[30px] overflow-hidden"
+  >
     <h3 class="mb-16 max-[1024px]:mb-12 max-[768px]:mb-7 text-white max-[430px]:text-[28px] max-[768px]:text-3xl max-[1240px]:text-4xl max-[570px]:text-left text-center max-[430px]:text-balance title">
       Real-world examples of how we have helped companies achieve their marketing objectives.
     </h3>
@@ -311,7 +324,10 @@
 </section>
 
 <div class="mx-auto px-4 max-w-[1408px]">
-  <div class="flex max-[570px]:flex-col justify-between items-center gap-15 max-[570px]:gap-9 bg-primary px-16 max-[430px]:px-4 max-[768px]:px-10 py-20 max-[570px]:py-14 rounded-[30px]">
+  <div 
+    style="background: url('<?php echo get_parent_theme_file_uri( 'assets/img/dark-background.webp' ) ?>')"
+    class="flex max-[570px]:flex-col justify-between items-center gap-15 max-[570px]:gap-9 !bg-primary !bg-cover !bg-no-repeat px-16 max-[430px]:px-4 max-[768px]:px-10 py-20 max-[570px]:py-14 rounded-[30px]"
+  >
     <h6 class="font-bold text-[80px] text-white max-[570px]:text-4xl max-[768px]:text-5xl max-[1024px]:text-6xl max-[1240px]:text-7xl max-[570px]:text-center">
       Ready to work with us?
     </h6>
